f5393f07696e609290eb0bf0d51b213b77b35049
SamratTheProgrammer <166220572+SamratTheProgrammer@users.noreply.github.com>
2024-09-11 00:35:56 +0530

    Add files via upload

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -94,7 +94,7 @@ closeBtn3.onclick = function() {
 //     delay: 200
 // });
 
-// ScrollReveal().reveal('.home-content , .heading', { origin: 'top' });
-// ScrollReveal().reveal('.home-img img , .about-img, .services-container , .portfolio-box, .contact form', { origin: 'bottom' });
-// ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
-// ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-content , .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img , .about-img, .services-container , .test-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
